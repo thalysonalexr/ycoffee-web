c0ca149f10596b6f8a38c514c885505da9d9dc2d
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-06-04 22:42:33 -0400

    adjust pages and add prettier

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
+import Footer from './Footer';
 import Header from './Header';
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
+import Navbar from './Navbar';
+import CardCoffee from './CardCoffee';
 import Loading from '../../components/Loading';
 import FormSearch from '../../components/FormSearch';
-import CardCoffee from '../../components/CardCoffee';
 import Pagination from '../../components/Pagination';
 
 import { CoffeeModel } from '../../types/domain/Coffee';
@@ -20,12 +20,10 @@ const Home: React.FC = () => {
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState('Oops! Nada para ver');
 
   const [coffees, setCoffees] = useState([] as CoffeeModel[]);
-  
-  const [isLoading, setIsLoading] = useState(false);
-  
-  const [message, setMessage] = useState('Oops! Nada para ver');
 
   async function listAllCoffees(page: number = 1) {
     setIsLoading(true);
@@ -48,7 +46,7 @@ const Home: React.FC = () => {
     const coffees = await listAllCoffees(pageNumber);
 
     setCoffees(coffees);
-  }
+  };
 
   const nextPage = async () => {
     if (page === totalPages) return;
@@ -57,23 +55,31 @@ const Home: React.FC = () => {
     const coffees = await listAllCoffees(pageNumber);
 
     setCoffees(coffees);
-  }
+  };
 
   async function listSearchedCoffees(query: string, input: string) {
     setIsLoading(true);
-    const { data } = await getAllCoffees<CoffeeModel>(query, input);
-    setCoffees(data.coffees);
-    setMessage(`Nenhum café encontrado para "${input}"`);
-    
+
+    try {
+      const { data } = await getAllCoffees<{ coffees: CoffeeModel[] }>(
+        query,
+        input
+      );
+
+      setCoffees(data.coffees);
+
+      setMessage(`Nenhum café encontrado para "${input}"`);
+    } catch {
+      setMessage(`Nenhum café encontrado para "${input}"`);
+    }
+
     setIsLoading(false);
   }
 
   useEffect(() => {
-    listAllCoffees().then((coffees) => {
-      setCoffees(coffees);
-    }).catch(err => {
-      setIsLoading(false);
-    });
+    listAllCoffees()
+      .then((coffees) => setCoffees(coffees))
+      .catch(() => setIsLoading(false));
   }, []);
 
   return (
@@ -90,21 +96,24 @@ const Home: React.FC = () => {
           {coffees.length > 0 ? (
             <div className="card-coffees">
               {coffees.map((coffee: CoffeeModel) => (
-                <CardCoffee key={coffee.id} data={coffee} />
+                <CardCoffee key={coffee.id} coffee={coffee} />
               ))}
             </div>
           ) : (
             <div className="wrapper-message">
               <span className="not-found">{message}</span>
-              <img src={bad} alt="Emoji triste"/>
+              <img src={bad} alt="Emoji triste" />
             </div>
           )}
         </div>
-        {coffees.length ? (<Pagination
-          currentPage={page}
-          totalPages={totalPages}
-          callbackNextPage={nextPage}
-          callbackPrevPage={prevPage} />) : null}
+        {coffees.length ? (
+          <Pagination
+            currentPage={page}
+            totalPages={totalPages}
+            callbackNextPage={nextPage}
+            callbackPrevPage={prevPage}
+          />
+        ) : null}
       </main>
       <Footer />
     </>
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaLongArrowAltRight, FaLongArrowAltLeft } from 'react-icons/fa';
+
+import Loading from '../../components/Loading';
 
 import { useAuth } from '../../contexts/auth';
 
@@ -8,7 +10,6 @@ import locks from '../../assets/icons/locks.svg';
 import signAt from '../../assets/icons/signat.svg';
 import signin from '../../assets/img/user-signin.webp';
 
-import '../sign.css';
 import './styles.css';
 
 const SignIn: React.FC = () => {
@@ -16,29 +17,34 @@ const SignIn: React.FC = () => {
   const [passw, setPassw] = useState('');
   const [error, setError] = useState('');
 
-  const { signIn } = useAuth();
+  const { signIn, loading, messageExpired } = useAuth();
 
   async function handleSignIn(e: React.FormEvent) {
     e.preventDefault();
 
-    const empty = [email, passw].some(input => input.length === 0);
+    const empty = [email, passw].some((input) => input.length === 0);
 
     if (empty) {
       setError('Preencha todos os campos.');
       return;
-    };
+    }
 
     try {
       await signIn(email, passw);
-    } catch({ response: { status } }) {
+    } catch ({ response: { status } }) {
       status === 401
         ? setError('Suas credenciais estão incorretas.')
         : setError('Sistema indisponível. Tente mais tarde.');
     }
   }
 
+  useEffect(() => {
+    if (messageExpired) setError(messageExpired);
+  }, [messageExpired]);
+
   return (
-    <main className="sign">
+    <>
+      <Loading loading={loading} />
       <div className="wrapper shadow-box">
         <div className="wrapper-nav">
           <Link title="Ir para página inicial" to={{ pathname: '/' }}>
@@ -46,16 +52,10 @@ const SignIn: React.FC = () => {
             voltar a Home
           </Link>
         </div>
-        {error
-          ? (<span className="error-message">
-              {error}
-            </span>)
-          : null}
+        {error ? <span className="error-message">{error}</span> : null}
         <div className="wrapper-content">
           <div className="wrapper-image">
-            <img
-              src={signin}
-              alt="Login de usuário" />
+            <img src={signin} alt="Login de usuário" />
           </div>
           <div className="wrapper-form">
             <form onSubmit={handleSignIn}>
@@ -64,7 +64,7 @@ const SignIn: React.FC = () => {
               </legend>
               <div className="wrapper-input">
                 <label htmlFor="email">
-                  <img src={signAt} alt="Icone de arroba"/>
+                  <img src={signAt} alt="Icone de arroba" />
                 </label>
                 <input
                   id="email"
@@ -74,11 +74,12 @@ const SignIn: React.FC = () => {
                   maxLength={100}
                   title="Insira seu endereço de e-mail"
                   placeholder="endereço de e-mail"
-                  onChange={(e) => setEmail(e.target.value)} />
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="wrapper-input">
                 <label htmlFor="passw">
-                  <img src={locks} alt="Icone de cadeado"/>
+                  <img src={locks} alt="Icone de cadeado" />
                 </label>
                 <input
                   id="passw"
@@ -87,16 +88,18 @@ const SignIn: React.FC = () => {
                   maxLength={255}
                   title="Insira sua senha"
                   placeholder="sua senha"
-                  onChange={(e) => setPassw(e.target.value)} />
+                  onChange={(e) => setPassw(e.target.value)}
+                />
               </div>
-              <button
-                type="submit"
-                className="shadow-box">
+              <button type="submit" className="shadow-box">
                 entrar
               </button>
             </form>
             <span>
-              <Link title="Registrar-se agora" to={{ pathname: '/signup' }}>
+              <Link
+                title="Registrar-se agora"
+                to={{ pathname: '/auth/signup' }}
+              >
                 Cadastre-se agora
                 <FaLongArrowAltRight size={16} color="#412507" />
               </Link>
@@ -104,8 +107,8 @@ const SignIn: React.FC = () => {
           </div>
         </div>
       </div>
-    </main>
+    </>
   );
-}
+};
 
 export default SignIn;
